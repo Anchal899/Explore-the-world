7b798f8ce4a468a1eea7e25ca1a2f78905f6c2c7
anchal <anchaljakhar75@gmail.com>
2023-09-26 14:45:02 +0530

    first commit

---
+++ about.js
@@ -0,0 +1,34 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+//about container
+ScrollReveal().reveal(".about_container .section_header",scrollRevealOption);
+ScrollReveal().reveal(".about_container .section_subheader",{
+...scrollRevealOption,
+delay: 500,
+});
+
+ScrollReveal().reveal(".about_container .about_flex",{
+    ...scrollRevealOption,
+    delay: 1000,
+    });
+
+    ScrollReveal().reveal(".about_container .btn",{
+        ...scrollRevealOption,
+        delay: 1500,
+        });
+
+        //discover content
+        ScrollReveal().reveal(".discover_card",{
+            ...scrollRevealOption,
+            interval:500,
+        });
+
+        ScrollReveal().reveal(".discover_card_content",{
+            ...scrollRevealOption,
+            interval:500,
+            delay:200,
+});
